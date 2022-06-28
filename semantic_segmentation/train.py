766f02a10de4fff5b8f93ab86398cbd5add6f94b
MURAKI Taichi <muraki.taichi@gmail.com>
2022-06-28 15:17:17 +0900

    fix

--- semantic_segmentation/train.py
+++ semantic_segmentation/train.py
@@ -3,16 +3,6 @@ import torch
 import flash
 from flash.image import SemanticSegmentation, SemanticSegmentationData
 
-
-# datamodule = SemanticSegmentationData.from_folders(
-#     train_folder="data/CameraRGB",
-#     train_target_folder="data/CameraSeg",
-#     val_split=0.1,
-#     transform_kwargs=dict(image_size=(256, 256)),
-#     num_classes=21,
-#     #
-# )
-
 datamodule = SemanticSegmentationData.from_folders(
     train_folder="custom_data/train_images/",
     train_target_folder="custom_data/train_masks/",
@@ -23,12 +13,12 @@ datamodule = SemanticSegmentationData.from_folders(
 )
 
 model = SemanticSegmentation(
-    # backbone="mobilenetv3_large_100",
-    # head="fpn",
+    backbone="mobilenetv3_large_100",
+    head="fpn",
     # backbone="xception",
     # head="unetplusplus",
-    backbone="efficientnet-b2",
-    head="unet",
+    # backbone="efficientnet-b2",
+    # head="unet",
     num_classes=datamodule.num_classes,
 )
 
